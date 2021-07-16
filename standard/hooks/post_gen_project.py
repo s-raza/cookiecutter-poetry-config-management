af1cb206edc75e0a5d4e7d4a8ef8847024786da5
s-raza+S20-Win <12784652+S20-Win+s-raza@users.noreply.github.com>
2021-07-17 00:32:06 +0400

    Add standard cookiecutter

---
+++ standard/hooks/post_gen_project.py
@@ -0,0 +1,66 @@
+import subprocess as sub
+import json
+import sys
+import os
+
+
+def run(msg, cmd_arr, stderr2out=False, print_output=False):
+
+    print(f'{msg} ...', end='', flush=True)
+
+    if stderr2out is False:
+        res = sub.run(cmd_arr, stdout=sub.PIPE, stderr=sub.PIPE, shell=True)
+
+        err = res.stderr.decode('latin1')
+
+        if err == '':
+            output = res.stdout.decode('latin1')
+            if print_output is True:
+                print(f'\n{output}\n')
+            print('done')
+            return output
+        else:
+            print(f'\nCommand run Error: {cmd_arr}\nError:\n{err}\n')
+            sys.exit(1)
+    else:
+        output = sub.run(cmd_arr, stdout=sub.PIPE, stderr=sub.STDOUT, shell=True).stdout.decode('latin1')
+        if print_output is True:
+            print(f'\n{output}\n')
+        print('done')
+        return output
+
+
+def write_pythonpath_vscode(pypath, vscode_config_path='.vscode/settings.json'):
+
+    with open(vscode_config_path, 'r') as f:
+        vscode_config = json.load(f)
+
+    vscode_config.setdefault('python', dict()).update({'pythonPath': pypath})
+
+    with open(vscode_config_path, 'w') as f:
+        json.dump(vscode_config, f, indent=4, sort_keys=True)
+
+
+print()
+poetry_install = run('Installing poetry virtual env', ['poetry', 'install'])
+
+poetry_output = poetry_install.split('\r\n')[0].split()
+venv_path = poetry_output[-1]
+venv_hash = poetry_output[-3]
+venv_full_path = os.path.join(venv_path, venv_hash)
+
+print(f'Poetry virtual env installed at: {venv_full_path}')
+
+venv_python = os.path.join(venv_full_path, 'Scripts', 'python.exe')
+
+git_init = run('Initializing GIT repo', ['git', 'init', '.'])
+pre_commit_install = run('Installing pre-commit', [venv_python, '-m', 'pre_commit', 'install'])
+pre_commit_autoupdate = run('Auto Updating pre-commit', [venv_python, '-m', 'pre_commit', 'autoupdate'])
+git_init = run('Adding project files to GIT repo', ['git', 'add', '.'])
+git_commit = run('Committing to GIT repo', ['git', 'commit', '-m', 'Initialize environment'], stderr2out=True)
+
+print('Updating vscode config with virtual env path ...', end='', flush=True)
+write_pythonpath_vscode(venv_python)
+print('done')
+
+sys.exit(0)
---
+++ standard/{{cookiecutter.project_name}}/config.py
@@ -0,0 +1,113 @@
+import glob
+import json
+import os
+from typing import Any, Dict, List
+from urllib.parse import quote_plus
+
+from dotenv import dotenv_values, load_dotenv
+
+load_dotenv()
+
+
+def __get_engine_string(database: Dict[str, Any]) -> str:
+
+    sqlite_file = database.get("sqlite")
+
+    if sqlite_file:
+        retval = "sqlite:///{}".format(sqlite_file)
+
+    else:
+
+        retval = (
+            database["dialect"]
+            + "://"
+            + database["user"]
+            + ":"
+            + quote_plus(database["password"])
+            + "@"
+            + database["host"]
+        )
+
+        if database["db_port"] is not None:
+            retval = retval + ":" + database["db_port"]
+
+        retval = retval + "/" + database["db_name"]
+
+        if database["db_options"] is not None:
+
+            retval = retval + "?"
+
+            for opt, val in database["db_options"].items():
+                retval = retval + opt + "=" + val + "&"
+
+    return retval
+
+
+def __get_json_config_files(dir: str) -> List[str]:
+
+    config_dir = f"{dir}/"
+    json_filter = os.path.join(config_dir, "**", "*.json")
+
+    return [f for f in glob.glob(json_filter, recursive=True)]
+
+
+def __get_dot_env_dict(
+    dotenv: Dict[str, Any], env_config_key: str
+) -> Dict[str, Any]:
+
+    ret_dict = {}
+
+    for key, val in dotenv.items():
+
+        if key.startswith(env_config_key):
+
+            applied_key = key.split(env_config_key)[1]
+
+            try:
+                ret_dict[applied_key] = json.loads(val)
+            except json.decoder.JSONDecodeError:
+                ret_dict[applied_key] = val
+
+    return ret_dict
+
+
+def __load_config_dir(conf_dir: str = "./config") -> None:
+
+    config_files_list = __get_json_config_files(conf_dir)
+    all_config: Dict[str, Any] = {}
+    dotenv_path: Any = ""
+
+    for f in config_files_list:
+
+        with open(f) as file_data:
+
+            file_config_dict = json.load(file_data)
+
+            for config_key, settings in file_config_dict.items():
+
+                if config_key in all_config:
+                    all_config[config_key].update(settings)
+                else:
+                    all_config[config_key] = settings
+
+    dotenv_path = all_config.get("dotenv_file_path")
+
+    dotenv = dotenv_values(dotenv_path)
+    dotenv_cfg_dict = __get_dot_env_dict(
+        dotenv, env_config_key=all_config["env_config_key"]
+    )
+
+    all_config.update(dotenv_cfg_dict)
+
+    if all_config.get("active_database") is not None:
+        all_config["database"] = all_config[all_config["active_database"]]
+        all_config["database"]["conn_string"] = __get_engine_string(
+            all_config["database"]
+        )
+    else:
+        all_config["database"] = None
+
+    globals().update(all_config)
+
+
+__load_config_dir()
