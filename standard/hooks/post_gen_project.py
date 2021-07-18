d527d9392d04d90494e662eff23bd857f8ddd03b
s-raza+S20-Win <12784652+S20-Win+s-raza@users.noreply.github.com>
2021-07-18 13:15:24 +0400

    Black: auto format

--- standard/hooks/post_gen_project.py
+++ standard/hooks/post_gen_project.py
@@ -15,28 +15,34 @@ def run(msg, cmd_arr, stderr2out=False, print_output=False):
 
     if stderr2out is False:
         res = sub.run(cmd_arr, stdout=sub.PIPE, stderr=sub.PIPE, shell=shell_)
-
         err = res.stderr.decode("latin1")
 
         if err == "":
             output = res.stdout.decode("latin1")
+
             if print_output is True:
                 print(f"\n{output}\n")
+
             print("done")
             return output
         else:
             print(f"\nCommand run Error: {cmd_arr}\nError:\n{err}\n")
             sys.exit(1)
     else:
-        output = sub.run(cmd_arr, stdout=sub.PIPE, stderr=sub.STDOUT, shell=shell_).stdout.decode("latin1")
+        output = sub.run(
+            cmd_arr, stdout=sub.PIPE, stderr=sub.STDOUT, shell=shell_
+        ).stdout.decode("latin1")
+
         if print_output is True:
             print(f"\n{output}\n")
+
         print("done")
         return output
 
 
-def write_pythonpath_vscode(pypath, vscode_config_path=".vscode/settings.json"):
-
+def write_pythonpath_vscode(
+    pypath, vscode_config_path=".vscode/settings.json"
+):
     with open(vscode_config_path, "r") as f:
         vscode_config = json.load(f)
 
@@ -60,10 +66,18 @@ python_subdir = "Scripts" if platform_ == "Windows" else "bin"
 venv_python = os.path.join(venv_full_path, python_subdir, "python")
 
 git_init = run("Initializing GIT repo", ["git", "init", "."])
-pre_commit_install = run("Installing pre-commit", [venv_python, "-m", "pre_commit", "install"])
-pre_commit_autoupdate = run("Auto Updating pre-commit", [venv_python, "-m", "pre_commit", "autoupdate"])
+pre_commit_install = run(
+    "Installing pre-commit", [venv_python, "-m", "pre_commit", "install"]
+)
+pre_commit_autoupdate = run(
+    "Auto Updating pre-commit", [venv_python, "-m", "pre_commit", "autoupdate"]
+)
 git_init = run("Adding project files to GIT repo", ["git", "add", "."])
-git_commit = run("Committing to GIT repo", ["git", "commit", "-m", "Initialize environment"], stderr2out=True)
+git_commit = run(
+    "Committing to GIT repo",
+    ["git", "commit", "-m", "Initialize environment"],
+    stderr2out=True,
+)
 
 print("Updating vscode config with virtual env path ...", end="", flush=True)
 write_pythonpath_vscode(venv_python)
