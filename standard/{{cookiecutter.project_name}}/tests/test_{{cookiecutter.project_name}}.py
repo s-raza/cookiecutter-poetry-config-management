5332bd57321ee674fcee0d19915e888386adf1cd
s-raza+S20-Win <12784652+S20-Win+s-raza@users.noreply.github.com>
2021-07-18 14:52:43 +0400

    Add test for existence of env_config_key in namespace of config.py

--- standard/{{cookiecutter.project_name}}/tests/test_{{cookiecutter.project_name}}.py
+++ standard/{{cookiecutter.project_name}}/tests/test_{{cookiecutter.project_name}}.py
@@ -1,5 +1,10 @@
 from {{cookiecutter.project_name}} import __version__
+import {{cookiecutter.project_name}}.config as cfg
 
 
 def test_version():
     assert __version__ == "0.1.0"
+
+
+def test_env_config_key_present():
+    assert hasattr(cfg, 'env_config_key')
