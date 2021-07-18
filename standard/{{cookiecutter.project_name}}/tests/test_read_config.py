953860036d5a6f98ae958677656c7e78e98e4c42
s-raza+S20-Win <12784652+S20-Win+s-raza@users.noreply.github.com>
2021-07-18 22:56:40 +0400

    Separate module for config tests, add db eng str tests

---
+++ standard/{{cookiecutter.project_name}}/tests/test_read_config.py
@@ -0,0 +1,35 @@
+from unittest import TestCase
+
+import {{cookiecutter.project_name}}.config as cfg
+
+
+def test_env_config_key_present():
+    assert hasattr(cfg, "env_config_key")
+
+
+def test_update_config_merge():
+
+    dict_to_update = {"master_k": {"k1": "v1", "k2": {"k2.1": "val2.1"}}}
+
+    dict_to_update_with = {"master_k": {"k3": {"k3.1": "val3.2"}}}
+
+    updated_dict = {
+        "master_k": {
+            "k1": "v1",
+            "k2": {"k2.1": "val2.1"},
+            "k3": {"k3.1": "val3.2"},
+        }
+    }
+
+    cfg.__update_config(dict_to_update, dict_to_update_with)
+    TestCase().assertDictEqual(dict_to_update, updated_dict)
+
+
+def test_update_config_blanks():
+
+    dict_to_update = {}
+    dict_to_update_with = {}
+    updated_dict = {}
+
+    cfg.__update_config(dict_to_update, dict_to_update_with)
+    TestCase().assertDictEqual(dict_to_update, updated_dict)
--- standard/{{cookiecutter.project_name}}/tests/test_{{cookiecutter.project_name}}.py
+++ standard/{{cookiecutter.project_name}}/tests/test_{{cookiecutter.project_name}}.py
@@ -1,10 +1,5 @@
-import {{cookiecutter.project_name}}.config as cfg
 from {{cookiecutter.project_name}} import __version__
 
 
 def test_version():
     assert __version__ == "0.1.0"
-
-
-def test_env_config_key_present():
-    assert hasattr(cfg, "env_config_key")
